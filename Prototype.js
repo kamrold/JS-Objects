406efbdf9e58beb1e6dda56135f7a689fd5e8f9e
Roland Bisasso <51475330+kamrold@users.noreply.github.com>
2019-06-20 15:50:16 +0300

    Add files via upload

---
+++ Prototype.js
@@ -0,0 +1,26 @@
+// Business Logic for AddressBook ---------
+function AddressBook() 
+{
+	this.contacts=[],
+	this.numId=0
+}
+function Contact(first_name, last_name, phone_number, email) 
+{
+  this.first_name=first_name,
+  this.last_name =last_name,
+  this.phone_number =phone_number,
+  this.email = email,
+ }
+  address = ("Street","City","country"){
+  this.street,
+  this.city,
+  this.country
+  }
+  Contact.prototype.full_name = function()
+ {
+
+console.log("full_name:" + this.first_name + " " + this.last_name);
+}
+var Contact1 =(David = new Contact (" Roland " + " Bisasso " + " 0755363699 " + " roland.bisasso@gmail.com " + " Nsabya " + "Kampala " + " Uganda ");
+
+Contact.full_name ();
